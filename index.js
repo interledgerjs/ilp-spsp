70892916c7a370c8eae36050048aebc17779dc35
Ben Sharafian <benms64@gmail.com>
2018-01-25 14:26:52 -0800

    feat: implement

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 #!/usr/bin/env node
+const plugin = require('ilp-plugin')()
+const SPSP = require('ilp-protocol-spsp')
+const debug = require('debug')('ilp-spsp')
 
-console.log('test')
+require('yargs')
+  .option('receiver', {
+    alias: 'r',
+    description: 'payment pointer of SPSP receiver'
+  })
+  .command('send', 'send money via SPSP', {
+    amount: {
+      alias: 'a',
+      required: true,
+      description: 'amount to send to receiver (source account base units)'
+    }
+  }, async argv => {
+    console.log(`paying ${argv.amount} to "${argv.receiver}"...`)
+
+    try {
+      debug('connecting plugin')
+      await plugin.connect()
+      debug('sending payment')
+      await SPSP.pay(plugin, {
+        receiver: argv.receiver,
+        sourceAmount: argv.amount
+      })
+    } catch (e) {
+      console.error(e)
+      process.exit(1)
+    }
+
+    console.log('sent!')
+    process.exit(0)
+  })
+  .command('query', 'query SPSP endpoint', {}, async argv => {
+    const response = await SPSP.query(argv.receiver)
+    response.sharedSecret = response.sharedSecret.toString('base64')
+    console.log(JSON.stringify(response, null, 2))
+    process.exit(0)
+  })
+  .argv
