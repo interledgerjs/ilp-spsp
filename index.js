3a5afcf28e636dc03a8fb553252664cb669d6315
Ben Sharafian <benms64@gmail.com>
2018-02-14 16:08:36 -0800

    feat: roll invoice into send

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ require('yargs')
   .command('send', 'send money via SPSP', {
     amount: {
       alias: 'a',
-      required: true,
       description: 'amount to send to receiver (source account base units)'
     }
   }, async argv => {
@@ -33,34 +32,6 @@ require('yargs')
 
     console.log('sent!')
     process.exit(0)
-  })
-  .command('invoice', 'pay an SPSP invoice', {}, async argv => {
-    console.log(`paying invoice at "${argv.receiver}"...`)
-
-    try {
-      debug('connecting plugin')
-      await plugin.connect()
-
-      debug('querying SPSP receiver')
-      const query = await SPSP.query(argv.receiver)
-
-      if (!query.balance) {
-        console.error('query result has no balance')
-        process.exit(1)
-      }
-
-      debug('paying invoice')
-      await PSK2.sendDestinationAmount(plugin, {
-        ...query,
-        destinationAmount: String(query.balance.maximum - query.balance.current)
-      })
-
-      console.log('paid!')
-      process.exit(0)
-    } catch (e) {
-      console.error(e)
-      process.exit(1)
-    }
   })
   .command('query', 'query SPSP endpoint', {}, async argv => {
     const response = await SPSP.query(argv.receiver)
