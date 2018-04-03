9d0a9a35dec5370b105aa86bf98e8870d3e49686
Ben Sharafian <benms64@gmail.com>
2018-04-03 09:43:10 -0700

    feat: use protocol spsp for invoice functionality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const plugin = require('ilp-plugin')()
 const SPSP = require('ilp-protocol-spsp')
-const PSK2 = require('ilp-protocol-psk2')
+const STREAM = require('ilp-protocol-stream')
 const debug = require('debug')('ilp-spsp')
 
 require('yargs')
@@ -12,11 +12,13 @@ require('yargs')
   .command('send', 'send money via SPSP', {
     amount: {
       alias: 'a',
-      required: true,
+      required: false,
       description: 'amount to send to receiver (source account base units)'
     }
   }, async argv => {
-    console.log(`paying ${argv.amount} to "${argv.receiver}"...`)
+    if (argv.amount) {
+      console.log(`paying (maximum of) ${argv.amount} to "${argv.receiver}"...`)
+    }
 
     try {
       debug('connecting plugin')
@@ -33,34 +35,6 @@ require('yargs')
 
     console.log('sent!')
     process.exit(0)
-  })
-  .command('invoice', 'pay an SPSP invoice', {}, async argv => {
-    console.log(`paying invoice at "${argv.receiver}"...`)
-
-    try {
-      debug('connecting plugin')
-      await plugin.connect()
-
-      debug('querying SPSP receiver')
-      const query = await SPSP.query(argv.receiver)
-
-      if (!query.balance) {
-        console.error('query result has no balance')
-        process.exit(1)
-      }
-
-      debug('paying invoice')
-      await PSK2.sendDestinationAmount(plugin, {
-        ...query,
-        destinationAmount: String(query.balance.maximum - query.balance.current)
-      })
-
-      console.log('paid!')
-      process.exit(0)
-    } catch (e) {
-      console.error(e)
-      process.exit(1)
-    }
   })
   .command('query', 'query SPSP endpoint', {}, async argv => {
     const response = await SPSP.query(argv.receiver)
